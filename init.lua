d57d1252b2833722a26d933962a74542943eab46
Ben Richards <benri@aq1systems.com>
2026-08-19 14:51:42 +1000

    Scratch based workflows

--- init.lua
+++ init.lua
@@ -7,3 +7,15 @@ vim.g.clipboad = "wl-copy"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+--- Mildly temporary, scratch file stuff
+vim.api.nvim_create_user_command("Scratch", function()
+  local filetype = vim.bo.buftype
+  vim.cmd("enew")
+  vim.bo.buftype = filetype
+  vim.bo.bufhidden = "hide"
+  vim.bo.swapfile = false
+end, {})
+
+-- Example keymap to open it with leader + ns
+vim.keymap.set("n", "<leader>ns", ":Scratch<CR>", { silent = true })
