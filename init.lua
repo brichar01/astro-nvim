dfaeb24abb55d2cb53bb2082b7400f30de2a2e0e
Ben Richards <benri@aq1systems.com>
2026-08-28 10:11:19 +1000

    Add vsplit to scratch command

--- init.lua
+++ init.lua
@@ -14,13 +14,13 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 --- Mildly temporary, scratch file stuff
 vim.api.nvim_create_user_command("Scratch", function()
-  local filetype = vim.bo.buftype
+  vim.cmd("vsplit")
   vim.cmd("enew")
-  vim.bo.buftype = filetype
+  vim.bo.filetype = "markdown"
+  vim.bo.buftype = "nofile"
   vim.bo.bufhidden = "hide"
   vim.bo.swapfile = false
 end, {})
 
 -- Example keymap to open it with leader + ns
 vim.keymap.set("n", "<leader>ns", ":Scratch<CR>", { silent = true })
-
